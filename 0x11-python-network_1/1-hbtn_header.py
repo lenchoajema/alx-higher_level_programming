2d15490757a0ad1dab7258d885078c1dcdb152a1
lencho <lenchoajema@gmail.com>
2023-12-02 13:45:42 -0800

    python network 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
 """
-given URL as parameter, fetch URL and display value from reponse headerder.py  https://alx-intranet.hbtn.io  nnnn 
-"""
-from sys import argv
-import urllib.request
 
 
 if __name__ == "__main__":
-        req = urllib.request.Request(argv[1])
-            with urllib.request.urlopen(req) as response:
-                        print(response.getheader('X-Request-Id'))
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        head = response.headers.get('X-Request-Id')
+        print(head)
